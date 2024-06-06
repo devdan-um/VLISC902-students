def81970bc5bc11511603715ea3d4420535748ac
BryaanEGL <btyan.elian@gmail.com>
2024-06-05 19:44:47 -0600

    Archivos para ir al corriente del entregable siguiente

---
+++ src/main/java/com/univer/students/entity/request/StudentRequest.java
@@ -0,0 +1,20 @@
+package com.univer.students.entity.request;
+
+public class StudentRequest {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentRequest{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
