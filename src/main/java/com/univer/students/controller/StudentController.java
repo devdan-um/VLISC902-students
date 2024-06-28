6e2fa7a256b2339478e88c97123d8be082b9d7b1
maria <1232100598@univermilenium.edu.mx>
2024-06-27 19:35:59 -0600

    entregable dos

--- src/main/java/com/univer/students/controller/StudentController.java
+++ src/main/java/com/univer/students/controller/StudentController.java
@@ -1,37 +1,19 @@
 package com.univer.students.controller;
 
-import com.univer.students.model.entity.StudentEntity;
-import com.univer.students.repository.StudentsRepository;
-import com.univer.students.request.StudentRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-
 public class StudentController {
 
-    @Autowired
-    private StudentsRepository studentsRepository;
-
     @GetMapping("student/register")
-
     public String student(){
         return "register";
-
-
     }
     @PostMapping("success")
     public String success(@ModelAttribute StudentRequest request){
-
-        StudentEntity estudiante = new StudentEntity();
-        estudiante.setName(request.getName());
-        this.studentsRepository.save(estudiante);
-
-        this.studentsRepository.findById(2);
-
         System.out.println(request);
         return "success";
     }
